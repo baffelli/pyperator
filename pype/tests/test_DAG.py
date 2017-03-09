5d19ccd68a54951f2d4555ae35ae4ee0a9b94959
baffelli <baffelli@ifu.baug.ethz.ch>
2017-03-09 09:33:24 +0100

    Added tests for DAG

---
+++ pype/tests/test_DAG.py
@@ -0,0 +1,26 @@
+from unittest import TestCase
+
+
+class TestDAG(TestCase):
+
+
+    def test_add_arc(self):
+        self.fail()
+
+    def test_remove_arc(self):
+        self.fail()
+
+    def test_iternodes(self):
+        self.fail()
+
+    def test_adjacent(self):
+        self.fail()
+
+    def test_iterarcs(self):
+        self.fail()
+
+    def test_dfs(self):
+        self.fail()
+
+    def test_dot(self):
+        self.fail()
