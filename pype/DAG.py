d32626bef4711995c60bde8675d8c4bf0a4c572c
baffelli <baffelli@ifu.baug.ethz.ch>
2017-03-15 16:37:31 +0100

    Now processing works.

--- pype/DAG.py
+++ pype/DAG.py
@@ -1,108 +1,8 @@
 import queue as _qu
 import textwrap as _tw
 
-def coroutine(func):
-    def start(*args,**kwargs):
-        cr = func(*args,**kwargs)
-        next(cr)
-        return cr
-    return start
-
-def process(fun):
-    def message_expander(message_list, **kwargs):
-        args=[message.data for message in message_list.items()]
-        ret = fun(*args, **kwargs)
-        return ret
-    return message_expander
-
-class message:
-    def __init__(self, content, originator):
-        self.data = content
-        self.originator = originator
-
-    def __repr__(self):
-        return "From: {}, data: {}".format(str(self.originator), self.data)
-
-class messageList(dict):
-
-    def originators(self):
-        return {r.originator for r in self if hasattr(r,'originator')}
-
-    def add(self, message):
-        if message:
-            super(messageList, self).__setitem__(message.originator,message)
-
-    def __repr__(self):
-        # return "".join([str(k) for k,v in self.items()])
-        strs = ["Originator : {r}, Data: {d}".format(r=k, d=v.data) for k,v in self.items()]
-        return "----\n".join(strs)
-
-
-class Node:
-    def __init__(self, name, f):
-        self.name = name
-        self._data = None
-        self._in = set()
-        self._out = set()
-        self.f = f
-        self.data = messageList()
-
-    def __repr__(self):
-        st = "{}".format(self.name)
-        return st
-
-    def add_outgoing(self,node):
-        self._out.add(node)
-
-    def add_incoming(self, node):
-        self._in.add(node)
-
-    def remove_outgoing(self, node):
-        self._out.remove(node)
-
-    def remove_incoming(self, node):
-        self._in.remove(node)
-
-    def send(self, message):
-        yield message
-
-    @coroutine
-    def __call__(self):
-        while True:
-            received_message = (yield)
-            self.data.add(received_message)
-            #Check if all messages are received
-            if self.data.originators() == set(self.incoming):
-                data = self.data.copy()
-                self.data.clear()
-            elif self.n_in ==0:
-                data = received_message
-            else:
-                continue
-            #When we have all messages, we compute something on them
-            transfomed = self.f(data)
-            #And then we send it to the descendent nodes
-            to_send = message(transfomed, self)
-            for c in self.outgoing:
-                    c().send(to_send)
-
-    @property
-    def n_in(self):
-        return len(self._in)
-
-    @property
-    def outgoing(self):
-        yield from self._out
-
-    @property
-    def incoming(self):
-        yield from self._in
-
-    @property
-    def has_predecessor(self):
-        if self._in:
-            return True
-
+from pype.nodes import Node
+from pype.utils import process
 
 cycle_str = 'Graph contains cycle beteween {} and {}'
 
@@ -221,16 +121,25 @@ class DAG:
             """.format(nodes="\n".join(nodes_gen), edges="\n".join(arc_str))
         return _tw.dedent(graph_str)
 
+    def  __call__(self):
+        #Find nodes with no predecessors
+        a = [el for el in self.iternodes() if not el.has_predecessor]
+        #Execute them
+        coros = [a() for a in a]
+        while True:
+                [next(coro) for coro in coros]
+            # except StopIteration:
+            #     print('Workflow complete')
+
+
 #Two generators
 simple_gen = (i for i in range(40))
-reverse_gen = (i for i in range(40,1,-1))
-
+reverse_gen = (2 for i in range(40))
+#A simple sum process
 @process
-def sum_messages(x):
-    if x is None:
-        return 1
-    else:
-        return sum(x)
+def sum_messages(a=None, b=None):
+    return a+b
+
 
 
 
@@ -239,60 +148,17 @@ a = Node('a', lambda x: next(simple_gen))
 b = Node('b', lambda x: next(reverse_gen))
 c = Node('c', sum_messages)
 d = Node('d', lambda x: print(x))
-spy = Node('spy', lambda x: print("I see {}".format(x)))
 graph = DAG()
 graph.add_arc(a, c)
 graph.add_arc(b, c)
 graph.add_arc(c, d)
-graph.add_arc(a,spy)
-graph.add_arc(b,spy)
-# graph.add_arc(b, c)
-# graph.add_arc(b, d)
-# d = b()
-# e = a()
-# f = c()
+#Execute
+graph()
+
+
+
 with open('a.dot', 'w+') as of:
     of.write(graph.dot())
-# a_g = a()
-# b_g = b()
-# next(b_g)
-# next(a_g)
 
-for a in zip(a(), b()):
-    pass
-# for a,b in zip(a(),b()):
-#     pass
-# e = a()
-# e.send(None)
-# d = b()
-# e = a()
-# f = c()
-# print(d)
-# e.send(None)
-# next(d)
-# next(d)
-# next(d)
 
-# c = Node('c')
-# d = Node('d')
-# e = Node('e')
-#
-# graph = DAG()
-#
-# # Add nodes
-# # graph.add_node(a)
-# # graph.add_node(b)
-# # graph.add_node(c)
-# # graph.add_node(d)
-# # graph.add_node(e)
-# # Connect them
-# graph.add_arc(a, b)
-# graph.add_arc(b, c)
-# graph.add_arc(c, d)
-# graph.add_arc(c, e)
-# # graph.add_arc(c, a)
-# # graph.remove_arc(c,c)
-# # print(list(find_path(graph, a,e)))
-# print(list(topological_sort(graph)))
-# with open('a.dot', 'w+') as of:
-#     of.write(graph.dot())
+
