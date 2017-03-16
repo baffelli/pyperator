5625cebaf075a1ba3195ed3c3f50ee8db18e6aa4
baffelli <baffelli@ifu.baug.ethz.ch>
2017-03-16 08:56:49 +0100

    Added installer for pip.

---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+from setuptools import setup
+setup(name='pype',
+      version='0.0',
+      description='Simple graph based push pipeline executor',
+      author='Simone Baffelli',
+      author_email='baffelli@ifu.baug.ethz.ch',
+      license='MIT',
+      include_package_data=True)
