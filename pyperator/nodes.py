287c6f4c06022e62af2898bab70d722eb3d09fff
baffelli <baffelli@ifu.baug.ethz.ch>
2017-03-21 10:53:32 +0100

    Defined call as a function that waits for input and send outputs.

--- pyperator/nodes.py
+++ pyperator/nodes.py
@@ -1,36 +1,15 @@
-from pyperator.utils import messageList, channel
-import asyncio
-
-
-
-
-class Port:
-
-    def __init__(self, name, size=1):
-        self.name = name
-        self._queue = asyncio.Queue(size)
-
-    async def send(self, data):
-        await self._queue.put(data)
-
-    async def receive(self):
-        data = self._queue.get()
-        return data
-
-    def connect(self, other):
-        self._queue = other._queue
-
+from .utils import Connection
+from .utils import Port
 
 class Component:
     def __init__(self, name, f=lambda x: None, inputs=[], outputs=[]):
         self.name = name
-        #Input and output ports
+        # Input and output ports
         self._inports = {}
         self._outports = {}
-        #Function of the node
-        self._f = f
+        # Function of the node
         self.color = 'grey'
-        #initalize ports
+        # initalize ports
         for inport in inputs:
             self._inports.update({inport: Port(inport)})
         for outport in outputs:
@@ -40,26 +19,52 @@ class Component:
         st = "{}".format(self.name)
         return st
 
+    def port_table(self):
+
+        port_template = "<TD PORT=\"{portname}\">{portname}</TD>"
+        row_template = "<TR>{ports}</TR>"
+        print(list(self.inports))
+        format_ports = lambda ports: "".join(port_template.format(portname=port.name) for port_name, port in ports)
+        inports = format_ports(self.inports)
+        outports = format_ports(self.outports)
+        inrow = row_template.format(ports=inports) if len(inports) > 0 else ""
+        outrow = row_template.format(ports=outports) if len(outports) > 0 else ""
+        table_template = """<
+                    <TABLE BORDER="0" CELLBORDER="1" CELLSPACING="0" CELLPADDING="0">
+                            {inrow}
+                            <TR><TD VALIGN="MIDDLE" COLSPAN="10" BGCOLOR="{color}">{name}</TD></TR>
+                            {outrow}
+                    </TABLE>>"""
+
+        return table_template.format(color=self.color, name=self.name, inports=inports, outports=outports, inrow=inrow,
+                                     outrow=outrow)
+
     def gv_node(self):
-        table_header = """<<TABLE BORDER="0" CELLBORDER="0" CELLSPACING="0" CELLPADDING="0"><TR><TD BORDER="0"><TABLE BORDER="0" CELLBORDER="0" CELLSPACING="0" CELLPADDING="0">"""
-        st = """
-                node[shape=record]
-                {name} [fillcolor={c}, label=\"{name}\", style=filled]""".format(c=self.color, name=str(self))
+        st = """node[shape=plaintext]
+                {name} [label={lab}]""".format(c=self.color, name=str(self), lab=self.port_table())
         return st
 
 
+    async def __call__(self):
+        #Receive
+        data = []
+        for p_name, p in self.inports:
+            received = await p.receive()
+            data.append(received)
+        transformed = self._f(data)
+        print(data)
+        #Send
+        for p_name, p in self.outports:
+            await p.send(transformed)
+
+
     def connect(self, other, outport, inport):
-        """
-        Connection protocol:
-        output port is always connect to input port of other node
-        """
-        #Check if the given port exists in this component
-        output_ports = [current_port for outport_name, current_port, in self.outports if outport_name == outport]
-        #Check if the given inport exists in the other component
-        input_ports = [inport for current_port, inport_name in other.inports if inport_name == inport]
-        if output_ports:
-            if input_ports:
-                output_ports[0].connect(input_ports)
+        c = Connection(self, other, outport, inport)
+        if outport in self._outports:
+            self._outports[outport].connect(c)
+        if inport in other._inports:
+            other._inports[inport].connect(c)
+        return c
 
 
     @property
@@ -73,17 +78,15 @@ class Component:
 
 class Node:
     def __init__(self, name, f=lambda x: None):
-        #Each node has a name
+        # Each node has a name
         self.name = name
-        #Nodes are stored as dicts
+        # Nodes are stored as dicts
         self._in = {}
         self._out = {}
-        #This is the function that the node will execute
+        # This is the function that the node will execute
         self.f = f
         self.data = []
         self.color = 'grey'
-
-
 
     def connect(self, other):
         pass
@@ -116,11 +119,10 @@ class Node:
                                                                               c.connection_exists(self, node)]
         return c[0]
 
-
     async def get_upstream(self):
         data = []
         if self.n_in > 0:
-            for incoming_node  in self.incoming:
+            for incoming_node in self.incoming:
                 print("{}, receiving on {}".format(self.name, incoming_node))
                 current_data = await self.channel.receive()
                 self.data.append(current_data)
@@ -131,42 +133,30 @@ class Node:
         if self.n_out > 0:
             for outgoing_node in self.outgoing:
                 print("{}, sending {} to {}".format(self.name, data, outgoing_node))
-                #send data to the node
+                # send data to the node
                 await outgoing_node.channel.send(data)
                 print('{} sent data downstream'.format(self.name))
                 await outgoing_node()
         return
 
-
-
     async def __call__(self, *args, **kwargs):
-            print('executing {}'.format(self.name))
-            if self.n_in > 0:
-                data = await self.get_upstream()
+        print('executing {}'.format(self.name))
+        if self.n_in > 0:
+            data = await self.get_upstream()
+        else:
+            data = None
+        print('{} processing data '.format(self.name))
+        if len(self.data) == self.n_in:
+            if self.n_in == 0:
+                transformed = self.f(data)
             else:
-                data = None
-            print('{} processing data '.format(self.name))
-            if len(self.data) == self.n_in:
-                if self.n_in ==0:
-                    transformed = self.f(data)
-                else:
-                    transformed = await self.f(self.data)
+                transformed = await self.f(self.data)
 
-                transformed = (self.name, transformed)
+            transformed = (self.name, transformed)
 
-                print("result {}".format(transformed))
-                await self.send_downstream(transformed)
-                self.data = []
-
-
-
-
-
-
-
-
-
-
+            print("result {}".format(transformed))
+            await self.send_downstream(transformed)
+            self.data = []
 
     @property
     def n_in(self):
