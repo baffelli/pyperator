198efb878d6c4ea961883974bb43327dec93c394
baffelli <baffelli@ifu.baug.ethz.ch>
2017-03-22 19:12:24 +0100

    Added a function that broadcasts the result of the computation to all downstream nodes.

--- pyperator/components.py
+++ pyperator/components.py
@@ -21,7 +21,22 @@ class GeneratorSource(Component):
         await self.close_downstream()
 
 
+class BroadcastApplyFunction(Component):
+    """
+    This component applies a function of all inputs
+    and sends it to all outputs
+    """
+    def __init__(self, name, function):
+        super(BroadcastApplyFunction,self).__init__(name)
+        self.function = function
 
+    async def __call__(self):
+        while True:
+            data = await self.receive()
+            transformed = self.function(**data)
+            data = {port_name: transformed for port_name, port in self.outputs.items()}
+            await asyncio.wait(self.send(data))
+            await asyncio.sleep(0)
 
 
 class ShowInputs(Component):
@@ -34,6 +49,6 @@ class ShowInputs(Component):
         while True:
             data = await self.receive()
             print(data)
-            # await asyncio.sleep(0)
+            await asyncio.sleep(0)
 
 
