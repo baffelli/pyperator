b388fff0e23d5243ffa5e216e93eb1ec21bf243e
baffelli <baffelli@ifu.baug.ethz.ch>
2017-03-22 18:57:56 +0100

    Added source component and printer component.

--- pyperator/components.py
+++ pyperator/components.py
@@ -1,4 +1,5 @@
 from .nodes import Component
+from .utils import Port, ArrayPort,InputPort, OutputPort
 import asyncio
 
 class GeneratorSource(Component):
@@ -6,29 +7,33 @@ class GeneratorSource(Component):
     This is a component that returns a single element from a generator
     to a single output
     """
-    def __init__(self, name, generator, outputs=[]):
-        super(GeneratorSource,self).__init__(name, generator, inputs=[], outputs=outputs)
+    def __init__(self, name, generator, output='OUT'):
+        super(GeneratorSource,self).__init__(name)
         self._gen = generator
+        self.outputs.add(OutputPort(output))
+
+    async def __call__(self):
+        for g in self._gen:
+            #We dont need to wait for incoming data
+            data = {port_name: g for port_name, port in self.outputs.items()}
+            await asyncio.wait(self.send(data))
+            await asyncio.sleep(0)
+        await self.close_downstream()
+
+
+
+
+
+class ShowInputs(Component):
+
+    def __init__(self, name, inputs=[]):
+        super(ShowInputs, self).__init__(name)
+        [self.inputs.add(InputPort(input)) for input in inputs]
 
     async def __call__(self):
         while True:
-            #We dont need to wait for incoming data
-            #We call the generator and send it output
-            gen_output = next(self._gen)
-            transformed = {out_name: gen_output for out_name, out_port in self.outputs}
-            await asyncio.wait(self.send(transformed))
-            await asyncio.sleep(0)
+            data = await self.receive()
+            print(data)
+            # await asyncio.sleep(0)
 
 
-
-
-
-class GeneratorProductSource(Component):
-    """
-    This is a component that returns
-    the product of the given generators
-    """
-
-
-class Sink(Component):
-    pass
