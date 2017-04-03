58ea0aee1ef61c3f5d7bc15fa1787c9afede39e3
baffelli <baffelli@ifu.baug.ethz.ch>
2017-04-03 17:33:58 +0200

    renamed method 'send' to 'send_all'

--- pyperator/components.py
+++ pyperator/components.py
@@ -3,6 +3,8 @@ from .utils import Port, ArrayPort,InputPort, OutputPort
 import asyncio
 import itertools
 
+import subprocess as _sub
+
 class GeneratorSource(Component):
     """
     This is a component that returns a single element from a generator
@@ -16,10 +18,10 @@ class GeneratorSource(Component):
     async def __call__(self):
         for g in self._gen:
             #We dont need to wait for incoming data
-            data = {port_name: g for port_name, port in self.outputs.items()}
-            await asyncio.wait(self.send(data))
+            await asyncio.wait(self.send_to_all(g))
             await asyncio.sleep(0)
-        await self.close_downstream()
+        # await self.close_downstream()
+
 
 
 class Split(Component):
@@ -28,7 +30,7 @@ class Split(Component):
     separate ouputs; the number of elements is given
     with `n_outs`
     """
-    def __init__(self, name, n_outs=2):
+    def __init__(self, name):
         super(Split, self).__init__(name)
         # [self.outputs.add(OutputPort('OUT{n}'.format(n=n))) for n in range(n_outs)]
         self.inputs.add(InputPort('IN'))
@@ -36,12 +38,9 @@ class Split(Component):
 
     async def __call__(self):
         while True:
-            data = await self.receive()
-            data = data.get('IN')
-            out_data = {}
+            data = await self.inputs.IN.receive()
             for index_port, ((data_item),(output_port_name, output_port)) in enumerate(zip(data, self.outputs.items())):
-                out_data[output_port_name] = data_item
-            await asyncio.wait(self.send(out_data))
+                asyncio.ensure_future(output_port.send(data_item))
             await asyncio.sleep(0)
 
 
@@ -71,8 +70,7 @@ class ConstantSource(Component):
             if self.repeat and i >= self.repeat:
                 return
             else:
-                data = {port_name: self.constant for port_name, port in self.outputs.items()}
-                await asyncio.wait(self.send(data))
+                await asyncio.wait(self.send_to_all(self.constant))
                 await asyncio.sleep(0)
 
 
@@ -93,7 +91,7 @@ class Filter(Component):
             #If the predicate is true, the data is sent
             if filter_result:
                 data = {port_name: data for port_name, port in self.outputs.items()}
-                await asyncio.wait(self.send(data))
+                await asyncio.wait(self.send_to_all(data))
             #otherwise nothing is sent and a message is sent  to
             #the components telling them that the filter failed
             else:
@@ -113,8 +111,7 @@ class BroadcastApplyFunction(Component):
         while True:
             data = await self.receive()
             transformed = self.function(**data)
-            data = {port_name: transformed for port_name, port in self.outputs.items()}
-            await asyncio.wait(self.send(data))
+            await asyncio.wait(self.send_to_all(transformed))
             await asyncio.sleep(0)
 
 
@@ -132,16 +129,15 @@ class OneOffProcess(BroadcastApplyFunction):
         data = await self.receive()
         while True:
             transformed = self.function(**data)
-            data = {port_name: transformed for port_name, port in self.outputs.items()}
-            await asyncio.wait(self.send(data))
+            data = transformed
+            await asyncio.wait(self.send_to_all(data))
             await asyncio.sleep(0)
 
 
 class ShowInputs(Component):
 
-    def __init__(self, name, inputs=[]):
+    def __init__(self, name):
         super(ShowInputs, self).__init__(name)
-        [self.inputs.add(InputPort(input)) for input in inputs]
 
     async def __call__(self):
         while True:
@@ -153,11 +149,23 @@ class ShowInputs(Component):
 class Shell(Component):
     """
     This component executes a shell script with inputs and outputs
+    the command can contain the following ports
     """
     def __init__(self, name, cmd):
         super(ShowInputs, self).__init__(name)
+        self.cmd = cmd
+        #
+        self.output_formatters = {}
 
+    def FixedFormatter(self, port, path):
+        self.output_formatters[port] = lambda self: path
 
+    def DynamicFormatter(self, inport, outport,):
+        self.output_formatters[outport] = lambda inpacket: inpacket.
 
-    def tofile(self):
-        pass
+    async def __call__(self):
+        #Wait for all upstram to be completed
+        await self.receive()
+        #Format shell command
+        cmd_formatted = self.cmd.format(self)
+        print(cmd_formatted)
