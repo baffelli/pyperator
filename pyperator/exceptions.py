580c23874615db536b36c9c4fe11d915c1f667c3
baffelli <baffelli@ifu.baug.ethz.ch>
2017-04-12 09:53:56 +0200

    All exceptions are renamed to SomethingError.

--- pyperator/exceptions.py
+++ pyperator/exceptions.py
@@ -1,79 +1,81 @@
-class ComponentException(BaseException):
+class ComponentError(BaseException):
     def __init__(self, message, component, *args):
         try:
             new_message = "Component {}: {}".format(component, message)
         except:
             new_message = 'fail'
-        super(ComponentException,self).__init__(new_message, *args)
+        super(ComponentError, self).__init__(message)
 
 
-class PortException(BaseException):
+class PortError(Exception):
     def __init__(self, message, channel, *args):
         try:
-            new_message = "Component {}: Port {} {}".format(channel.component, channel.name, message)
+            new_message = "Component {component}: Port {message=name} {}".format(component=channel.component, name=channel.name, message=message)
         except:
             new_message = 'fail'
-        super(PortException,self).__init__(new_message, *args)
+        super(PortError, self).__init__()
+        self.message = new_message
 
 
-class PortNotExistingException(ComponentException):
+class PortNotExistingError(ComponentError):
     def __init__(self, component, item, *args):
-        super(PortNotExistingException, self).__init__("port {} does not exist.".format(item), component)
+        ComponentError.__init__(self, "port {} does not exist.".format(item), component, *args)
 
 
 class StopComputation(StopIteration):
     pass
 
 
-class PortDisconnectedError(PortException):
+class PortDisconnectedError(PortError):
     def __init__(self, channel, *args):
         super(PortDisconnectedError, self).__init__("is disconnected", channel)
 
 
-class OutputOnlyError(PortException):
+class OutputOnlyError(PortError):
     def __init__(self, channel, *args):
         super(OutputOnlyError, self).__init__("is a OutputPort, it cannot be used to receive.", channel, *args)
 
 
-class InputOnlyError(PortException):
+class InputOnlyError(PortError):
     def __init__(self, channel, *args):
         super(InputOnlyError, self).__init__("is a InputPort, it cannot be used to send.", channel, *args)
 
 
-class MultipleConnectionError(BaseException):
-    pass
+class MultipleConnectionError(PortError):
+    def __init__(self, channel, *args):
+        super(MultipleConnectionError, self).__init__("an InputPort only supports one incoming connection.", channel, *args)
 
 
-class PortClosedError(PortException):
+class PortClosedError(PortError):
     def __init__(self, channel, *args):
         super(PortClosedError, self).__init__("is closed", channel, *args)
 
 
-class FormatterError(BaseException):
+class FormatterError(Exception):
     def __init__(self, *args, **kwargs):
         BaseException.__init__(self, *args, **kwargs)
 
 
-class WildcardNotExistingError(BaseException):
+class WildcardNotExistingError(Exception):
     def __init__(self, *args, **kwargs):
         BaseException.__init__(self, *args, **kwargs)
 
 
-class CommandFailedError(ComponentException):
+class CommandFailedError(ComponentError):
     def __init__(self,component, message, *args):
-        ComponentException.__init__(self, message, component)
+        ComponentError.__init__(self, message, component)
 
 
-class FileExistingError(BaseException):
+class FileExistingError(Exception):
     def __init__(self, *args, **kwargs):
         BaseException.__init__(self, *args, **kwargs)
 
 
-class FileNotExistingError(BaseException):
+class FileNotExistingError(Exception):
     def __init__(self, *args, **kwargs):
         BaseException.__init__(self, *args, **kwargs)
 
 
-class PacketOwnedError(BaseException):
+class PacketOwnedError(Exception):
     def __init__(self, *args, **kwargs):
         BaseException.__init__(self, *args, **kwargs)
