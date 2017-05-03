1932aba9dc3c904bb7b5767236a055013989abbb
baffelli <baffelli@ifu.baug.ethz.ch>
2017-05-03 19:11:16 +0200

    Added __init__ to facilitate import of important functions and decorators.

---
+++ pyperator/__init__.py
@@ -0,0 +1,5 @@
+from pyperator.decorators import inport, outport, component
+from pyperator.nodes import Component
+from pyperator.DAG import Multigraph
+from pyperator.utils import InputPort, OutputPort
+import pyperator.components
