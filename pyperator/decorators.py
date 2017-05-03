3688b84b5772b3b5e190a71b461e83cec421e0ff
baffelli <baffelli@ifu.baug.ethz.ch>
2017-05-03 18:23:18 +0200

    Added decorator to create component from async function definition.

--- pyperator/decorators.py
+++ pyperator/decorators.py
@@ -1,3 +1,7 @@
+
+from pyperator.utils import InputPort, OutputPort
+from pyperator.nodes import Component
+
 def log_schedule(method):
     def inner(instance):
         try:
@@ -7,3 +11,29 @@ def log_schedule(method):
         return method(instance)
 
     return inner
+
+
+
+def component(func):
+    def inner(*args, **kwargs):
+        new_c = type(func.__name__,(Component,), {'__call__':func})
+        return new_c
+    return inner
+
+
+def inport(portname,**portopts):
+    def inner_dec(func):
+        def wrapper(*args, **kwargs):
+            c1 = func()(*args, **kwargs)
+            c1.inputs.add(InputPort(portname,**portopts))
+            return c1
+        return wrapper
+    return inner_dec
+
+def outport(portname, **portopts):
+    def inner_dec(func):
+        def wrapper(*args, **kwargs):
+            func(*args, **kwargs)
+            args[0].outputs.add(OutputPort(portname,**portopts))
+        return wrapper
+    return inner_dec
