7406dd1594a5e622e27474483c845e57e858f40d
baffelli <baffelli@ifu.baug.ethz.ch>
2017-05-08 18:52:44 +0200

    Adding decorators to the import

--- pyperator/__init__.py
+++ pyperator/__init__.py
@@ -1,4 +1,4 @@
-from pyperator.decorators import inport, outport, component
+from pyperator.decorators import inport, outport, component, run_once
 from pyperator.nodes import Component
 from pyperator.DAG import Multigraph
 from pyperator.utils import InputPort, OutputPort
