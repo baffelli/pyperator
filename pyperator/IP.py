3ec290af7fe6ca0c742fb81ed3767f9a5fcf47f8
baffelli <baffelli@ifu.baug.ethz.ch>
2017-05-09 17:54:11 +0200

    Added a textual representation of open and closed brackets.

--- pyperator/IP.py
+++ pyperator/IP.py
@@ -11,7 +11,7 @@ class InformationPacket(object):
         del self
 
     def __str__(self):
-        return "{} owned by {}, payload {}, payload type {}".format(self.__repr__(), self.owner, self.value, type(self.value))
+        return "{} owned by {}, payload {}, payload type {}".format(self.__repr__(), self.owner, self.value.__repr__(), type(self.value))
 
     @property
     def value(self):
@@ -60,13 +60,7 @@ class EndOfStream(InformationPacket):
         return "EOS"
 
 
-class Bracket(InformationPacket):
-    """
-    This is a bracket IP, composed of a list of IPs
-    """
 
-    def __init__(self, owner=None):
-        super(Bracket, self).__init__(value=None, owner=owner)
 
 
 class OpenBracket(InformationPacket):
@@ -74,8 +68,13 @@ class OpenBracket(InformationPacket):
     def __init__(self, owner=None):
         super(OpenBracket, self).__init__(value=None, owner=owner)
 
+    def __repr__(self):
+        return "("
+
 
 class CloseBracket(InformationPacket):
     def __init__(self, owner=None):
         super(CloseBracket, self).__init__(value=None, owner=owner)
 
+    def __repr__(self):
+        return ")"
